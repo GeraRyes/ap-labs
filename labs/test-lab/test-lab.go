db2b5d82bf9c27533cc11b37b499690c8eb84e09
Katacoda Scenario <scenario@katacoda.com>
2021-02-16 20:30:35 +0000

    Solve test-lab
    Signed-off-by: Katacoda Scenario <scenario@katacoda.com>

--- labs/test-lab/test-lab.go
+++ labs/test-lab/test-lab.go
@@ -2,8 +2,18 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Welcome to the jungle")
+  args := os.Args[1:]
+  if (len(args)==0){
+    fmt.Println("Error")
+  }else{
+    for i := 0; i < len(args); i++ {
+		fmt.Print(args[i]+" ")
+	  }
+	  fmt.Println("is in advanced programming")
+  }
 }
+  
