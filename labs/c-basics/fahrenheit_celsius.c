a3a71aacdbf3f697573cf7f8476ef57c93af527e
GeraRyes <gerarys213@gmail.com>
2021-02-23 00:06:30 +0000

    Solved c-basics lab
    Signed-off-by: GeraRyes <gerarys213@gmail.com>

--- labs/c-basics/fahrenheit_celsius.c
+++ labs/c-basics/fahrenheit_celsius.c
@@ -6,12 +6,36 @@
 
 /* print Fahrenheit-Celsius table */
 
-int main()
+int main(int argc, char **argv)
 {
-    int fahr;
+    if (argc==1){
+        printf("Please add another parameter to perform calculations");
+        return 1;
+    }else if (argc==2){
+        char *a = argv[1];
+        int fahr = atoi(a);
+        printf("Fahrenheit: %3d, Celcius: %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
 
-    for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
-	printf("Fahrenheit: %3d, Celcius: %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+    }else if(argc==4){
+        int fahr;
 
-    return 0;
+        char *a = argv[1];
+        int low = atoi(a);
+
+        char *b = argv[2];
+        int high = atoi(b);
+
+        char *c = argv[3];
+        int stp = atoi(c);
+
+
+        for (fahr = low; fahr <= high; fahr = fahr + stp)
+	    printf("Fahrenheit: %3d, Celcius: %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+
+        return 0;
+    }else{
+        printf("Error, numero incorrecto de parametros");
+        return 1;
+    }
+    
 }
