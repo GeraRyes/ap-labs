ddb4e949994e7e31d82e640cbd6e4121ee55fb5f
GeraRyes <gerarys213@gmail.com>
2021-03-07 09:10:28 +0000

    Porqué tantas segmentation fault?

--- challenges/first-partial/pacman-analyzer.c
+++ challenges/first-partial/pacman-analyzer.c
@@ -4,18 +4,165 @@
 #include <stdlib.h>
 #include <stdbool.h> 
 
-
+//CAPACIDAD DE LA HASHTABLE
+#define CAPACITY 400 
 #define REPORT_FILE "packages_report.txt"
+
+
+//ESTOS SON NUESTROS OBJETOS QUE CONTIENEN LA INFORMACION DE LOS PAQUETES
+typedef struct packageData packageData;
 
 struct packageData {
     char *pkgName;
-    char *scriptType;
     char *installDate;
     char *lastUpdate;
     char *removalDate;
     int howManyUpdates;
-    int key;
+    bool isInstalled;
 };
+
+//ESTO DEFINE NUESTRA HASHTABLE
+typedef struct HashTable HashTable;
+ 
+
+struct HashTable {
+
+    packageData** items;
+    int size;
+    int count;
+};
+
+//SE CREA UNA NUEVA HASTABLE
+HashTable* create_table(int size) {
+   
+    HashTable* table = (HashTable*) malloc (sizeof(HashTable));
+    table->size = size;
+    table->count = 0;
+    table->items = (packageData**) calloc (table->size, sizeof(packageData*));
+    for (int i=0; i<table->size; i++)
+        table->items[i] = NULL;
+ 
+    return table;
+}
+ 
+
+//SE CREA UN NUEVO ITEM DE TIPO DATA DE PAQUETE
+packageData* createItem(char *pkgName, char *date, int operationType) {
+    
+    
+    packageData* item = (packageData*) malloc (sizeof(packageData));
+    item->pkgName = (char*) malloc (strlen(pkgName) + 1);
+    strcpy(item->pkgName, pkgName);
+
+    if (operationType==0){
+        item->installDate = (char*) malloc (strlen(date) + 1);
+        
+        strcpy(item->installDate, date);
+    }else if (operationType==1){
+        item->removalDate = (char*) malloc (strlen(date) + 1);
+        strcpy(item->removalDate, date);
+    }else if (operationType==2){
+        item->lastUpdate = (char*) malloc (strlen(date) + 1);
+        strcpy(item->lastUpdate, date);
+    }
+    return item;
+}
+
+//FUNCIÓN QUE SIRVE PARA HACER EL HASHING Y REGRESA LA POSICION
+unsigned long hash_function(char* str) {
+    unsigned long i = 0;
+    for (int j=0; str[j]; j++){
+        i += str[j];
+    }
+    return i % CAPACITY;
+}
+
+void handle_collision(HashTable* table, unsigned long index, packageData* item) {
+}
+ 
+void ht_insert(HashTable* table, packageData *item, int operationType) {
+
+    // Compute the index
+    unsigned long index = hash_function(item->pkgName);
+ 
+    packageData* currentItem = table->items[index];
+     
+    if (currentItem == NULL) {
+        // Key does not exist.
+        if (table->count == table->size) {
+            // Hash Table Full
+            printf("Insert Error: Hash Table is full\n");
+
+            return;
+        }
+         
+        // Insert directly
+        table->items[index] = item; 
+        table->count++;
+    }
+ 
+    else {
+            // Scenario 1: We only need to update value
+            if (strcmp(currentItem->pkgName, item->pkgName) == 0) {
+
+                if (strcmp(operationType, "removed") == 0){
+                    
+                    strcpy(table->items[index]->removalDate, item->removalDate);
+                    table->items[index]->isInstalled=false;
+                    return;
+                    
+                }else if (strcmp(operationType, "installed") == 0 || strcmp(operationType, "reinstalled") == 0){
+                    
+                    strcpy(table->items[index]->installDate, item->installDate);
+                    table->items[index]->isInstalled=true;
+                    return;
+                    
+                }else if (strcmp(operationType, "upgraded") == 0 ){
+                    
+                    strcpy(table->items[index]->lastUpdate, item->lastUpdate);
+                    table->items[index]->howManyUpdates++;
+                    table->items[index]->isInstalled=true;
+                    return;
+                    
+                }{
+
+                }
+                
+            }
+     
+        else {//AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+            // Scenario 2: Collision
+            // We will handle case this a bit later
+            handle_collision(table, index, item);
+            return;
+        }
+    }
+}
+ 
+packageData* htSearch(HashTable* table, char* pkgName) {
+    // Searches the key in the hashtable
+    // and returns NULL if it doesn't exist
+    int index = hash_function(pkgName);
+    packageData* item = table->items[index];
+ 
+    // Ensure that we move to a non NULL item
+    if (item != NULL) {
+        if (strcmp(item->pkgName, pkgName) == 0)
+            return item;
+    }
+    return NULL;
+}
+
+void print_table(HashTable* table) {
+    printf("\nHash Table\n-------------------\n");
+    for (int i=0; i<table->size; i++) {
+        if (table->items[i]) {
+            printf("Index:%d, Nombre Paquete:%s\n", i, table->items[i]->pkgName);
+        }
+    }
+    printf("-------------------\n\n");
+}
+
 
 void analizeLog(char *logFile, char *report);
 
@@ -27,7 +174,6 @@ int main(int argc, char **argv) {
     }
 
     analizeLog(argv[2], REPORT_FILE);
-
     return 0;
 }
 
@@ -39,9 +185,11 @@ void analizeLog(char *logFile, char *report) {
     size_t len = 0;
     ssize_t read;
 
+    int numberALPM=0, numberPACMAN=0, numberALPMS=0, operationType;
+    char *pkgName, *date, *isInstalled, *wasUpgraded, *howManyUpgrades;
     
-    int numberALPM=0, numberPACMAN=0, numberALPMS=0;
-    char *pkgName, *scriptType, *date, *operationType, *isInstalled, *wasUpgraded, *howManyUpgrades; 
+    HashTable* ht = create_table(CAPACITY);
+    
 
     fp = fopen(logFile, "r");
     if (fp == NULL)
@@ -52,12 +200,14 @@ void analizeLog(char *logFile, char *report) {
         char *storedChar;
         bool desechar=false;
         int from, to, i, j;
+        bool isRemoved=false;
         
         
         if (line[0]=='['){
 
             //TOMA EL VALOR DE LA FECHA
             storedChar = strtok(line, "]");
+
             from=1;
             to=10;
             date=(char*)malloc ( 11*sizeof(char) );
@@ -67,18 +217,14 @@ void analizeLog(char *logFile, char *report) {
 		        date[j]=storedChar[i];
                 
             }
-            printf("%s\n", date);
-        
-            
 
             //TOMA EL VALOR DEL TIPO DE SCRIPT 
             storedChar = strtok(NULL, " ");
-            scriptType=(char*)malloc ( 11*sizeof(char) );
 
             //CONTAR EL SCRIPT PARA LOS VALORES FINALES
-            if (storedChar=="[PACMAN]"){
+            if ((strcmp(storedChar, "[PACMAN]") == 0)){
                 numberPACMAN++;
-            }else if(storedChar=="[ALPM]"){
+            }else if((strcmp(storedChar, "[ALPM]") == 0)){
                 numberALPM++;
             }else{
                 numberALPMS++;
@@ -86,133 +232,44 @@ void analizeLog(char *logFile, char *report) {
 
             //TIPO DE OPERACION(INSTALLED, REINSTALLED, REMOVED, UPGRADED)
             storedChar = strtok(NULL, " ");
-            printf("%s ", storedChar);
 
-            if (storedChar=="installed" || storedChar=="reinstalled" || storedChar=="removed" || storedChar=="upgraded"){
+            
 
+            if (strcmp(storedChar, "installed") == 0 || strcmp(storedChar, "reinstalled") == 0 || strcmp(storedChar, "removed") == 0 || strcmp(storedChar, "upgraded") == 0){
+                
+                if (strcmp(storedChar, "installed") == 0 || strcmp(storedChar, "reinstalled") == 0){
+                    operationType=0;
+                }else if (strcmp(storedChar, "removed") == 0){
+                    operationType=1;
+                }else if (strcmp(storedChar, "upgraded") == 0){
+                    operationType=2;
+                }else{
+                    printf("error");
+                }
+                
                 //NOMBRE DEL PKG
                 storedChar = strtok(NULL, " ");
-                printf("%s\n", storedChar);
+                pkgName=storedChar;
+
+                printf("%s %s %i\n ", date, pkgName, operationType);
+
+                packageData* item = createItem(pkgName, date, operationType);
+                ht_insert(ht, item, operationType);
                 
-            }else{
-                desechar=true;
             }
 
             
 
         }
         
-        
-        /*
-
-        int currentChar=1;
-
-        bool inPkgType=false;
-        bool isCommonString=false;
-
-        
-        
-        
-        //CONSEGUIR LA FECHA
-        for (currentChar;currentChar<11;currentChar++){
-            char *tmp=(char *) malloc(strlen(strDate));
-            strcpy(tmp, strDate);
-            strDate = (char *) malloc(1 + strlen(strDate));
-            strcpy(strDate, tmp);
-            strDate[currentChar]=line[currentChar];
-            free(tmp);
-        }
-
-        strcpy(date, strDate);
-        free(strDate);
-        printf("%s\n", date);
-
-        //CONSEGUIR EL TIPO
-        for (currentChar;currentChar<read;currentChar++){
-            
-            if (inPkgType){
-                if (line[currentChar+4] =='-'){
-                    pkgType="ALPM-SCRIPLET";
-                    numberALPMS++;
-                    currentChar+=13;
-                    inPkgType=false;
-                    break;
-                }else if (line[currentChar] =='A'){
-                    pkgType="ALPM";
-                    numberALPM++;
-                    currentChar+=4;
-                    inPkgType=false;
-                    break;
-                }else{
-                    pkgType="PACMAN";
-                    numberPACMAN++;
-                    currentChar+=6;
-                    inPkgType=false;
-                    break;
-                }
-            }
-
-            if (line[currentChar] == '['){
-                inPkgType=true;
-            }
-        }
-
-        printf("%s\n", pkgType);
-        
-
-        
-        //CONSEGUIR INSTALLED (REINSTALLED), UPGRADED OR REMOVED
-        
-        if (line[currentChar] =='i'  && line[currentChar+1] =='n' && line[currentChar+2] =='s' && line[currentChar+3] =='t'){
-            strcpy(operationType,"installed");
-            currentChar+=9;
-                
-        }else if (line[currentChar] =='r'  && line[currentChar+1] =='e' && line[currentChar+2] =='i' && line[currentChar+3] =='n'){
-            strcpy(operationType,"installed");
-            currentChar+=11;
-            
-        }else if (line[currentChar] =='r'  && line[currentChar+1] =='e' && line[currentChar+2] =='m' && line[currentChar+3] =='o'){
-            strcpy(operationType,"removed");
-            currentChar+=7;
-        }else if (line[currentChar] =='u'  && line[currentChar+1] =='p' && line[currentChar+2] =='g' && line[currentChar+3] =='r'){
-            strcpy(operationType,"upgraded");
-            currentChar+=8;
-        }else{
-            isCommonString=true;
-        }
-
-        //CONSEGUIR PKGNAME
-        int counter=0;
-        int tmpCounter=1;
-        char *tmpCounter
-       
-        while (line[currentChar]!=' '){
-            //char *tmp =  malloc(tmpCounter * sizeof(char*));
-            /*strcpy(tmp2, strPkgName);
-            strPkgName = (char *) malloc(1 + strlen(strPkgName));
-            strcpy(strPkgName, tmp2);
-            strDate[counter]=line[currentChar];
-            counter++;
-            currentChar++;
-            */
-        }
-            //strcpy(pkgName, strPkgName);
-        
-        /*if (operationType="install"){
-            
-        }
     }
-
-    printf("Number of ALPM: %i\n Number of ALPM-SCRIPLET: %i\n Number of PACMAN: %i\n", numberALPM, numberALPMS, numberPACMAN);
-        
-    */
 
     fclose(fp);
     if (line)
         free(line);
     exit(EXIT_SUCCESS);
 
-
+    print_table(ht);
     printf("Report is generated at: [%s]\n", report);
 }
 
